e1dfe3a51837afdf2dec3d7e70760086c2b56f9f
navsingh2k <snavneet795@gmail.com>
2022-12-08 10:05:46 +0530

    final

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cryptocurrency Tracker with React JS, Material UI and Chart JS
+
+
+
+
+
+## Built with 
+
+- [React JS](https://reactjs.org/)
+- [Material UI](https://v4.mui.com/)
+- [Chart JS](https://reactchartjs.github.io/react-chartjs-2/#/)
+- [Firebase](https://firebase.google.com/)
+
+created By: Shubham Bharti
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDkztoZXCTXbwYs6GMbS2ejS46c7Rp05nM",
+  authDomain: "crypto-hunter-22fb0.firebaseapp.com",
+  projectId: "crypto-hunter-22fb0",
+  storageBucket: "crypto-hunter-22fb0.appspot.com",
+  messagingSenderId: "614625386556",
+  appId: "1:614625386556:web:cf4234f7911cb05ffae339"
+};
+
+export default firebaseConfig;
