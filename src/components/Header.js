0ad693c23b3f05d10d2d9e0ca3222813c6826650
navsingh2k <snavneet795@gmail.com>
2022-12-18 21:50:30 +0530

    crypto-tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Hunter
+              Crypto Tracker
             </Typography>
             <Select
               variant="outlined"
